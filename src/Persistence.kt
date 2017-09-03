f0db0080dcdd51f5645ba248de1b10eaebd7f366
dp118m <dp@altruix.co>
2017-09-03 13:57:24 +0300

    Completed the example.

--- src/Persistence.kt
+++ src/Persistence.kt
@@ -1,2 +1,29 @@
-class Persistence {
+open class Persistence {
+    var db:IDatabase? = null
+
+    fun init():Outcome<Boolean> {
+        // Initialize the database connection here
+        // In case of success, put the database object into this.db
+        return Outcome(true, "")
+    }
+    fun someDatabaseOperation(input:String):Outcome<String> {
+        val db = this.db
+        if (db == null) {
+            // Test case 1
+            return Outcome(false, "Internal error")
+        }
+        try {
+            return someDatabaseOperationLogic(db, input) // Test case 2
+        }
+        catch (exception:Throwable) {
+            // Test case 3
+            return Outcome(false, exception.message ?: "")
+        }
+    }
+
+    private fun someDatabaseOperationLogic(db: IDatabase, input: String): Outcome<String> {
+        // Do actual operations with the database here
+        val result = "Some result"
+        return Outcome<String>(true, "", result)
+    }
 }
